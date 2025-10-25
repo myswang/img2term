c5a366abcd86bfdb2e607dc83144abe6d2bef252
Mike Wang <mike.wang7@gmail.com>
2025-10-24 22:09:50 -0700

    add toggle for status bar

--- src/img2term/img2term.py
+++ src/img2term/img2term.py
@@ -11,6 +11,7 @@ term = Terminal()
 file_formats = {f"{ext.lower()}" for ext in Image.registered_extensions().keys()}
 resize_detected = False
 needs_render = True
+show_status = True
 img_idx = 0
 file_names = []
 images = []
@@ -44,7 +45,10 @@ def resize_img(img):
     img_width = img.width
     img_height = img.height
     max_width = term.width
-    max_height = 2 * (term.height - 1) # allow space for printing at bottom
+    max_height = 2 * (term.height - 1)
+    global show_status
+    if not show_status:
+        max_height += 2
 
     # do not scale the image if its smaller than max_width/height
     if img_width <= max_width and img_height <= max_height:
@@ -75,7 +79,11 @@ def render_img(img):
     composited, height, width = img_to_array(img)
     output = []
     mid_x = (term.width - width) // 2
-    mid_y = (term.height * 2 - height) // 4
+    global show_status
+    vy = term.height
+    if show_status:
+        vy -= 1
+    mid_y = (vy * 2 - height) // 4
     for y in range(0, height, 2):
         line = []
         for x in range(width):
@@ -102,7 +110,11 @@ def render_img_offset(img):
     composited, height, width = img_to_array(img)
     output = []
     mid_x = (term.width - width) // 2
-    mid_y = (term.height * 2 - height) // 4
+    vy = term.height
+    global show_status
+    if show_status:
+        vy -= 1
+    mid_y = (vy * 2 - height) // 4
     y = 0
     screen_y = 0
     while y < height:
@@ -125,7 +137,7 @@ def render_img_offset(img):
                 tr, tg, tb = top
                 line.append(term.color_rgb(int(tr), int(tg), int(tb)) + "▀")
 
-        output.append(term.move_xy(mid_x, mid_y + (screen_y // 2) - 1) + "".join(line) + term.normal)
+        output.append(term.move_xy(mid_x, mid_y + (screen_y // 2)) + "".join(line) + term.normal)
         y += 1
         screen_y += 2
         if y > 1:
@@ -148,15 +160,16 @@ def on_resize(signum, frame):
 
 
 def main():
-    parser = argparse.ArgumentParser(description="View images in your terminal")
-    parser.add_argument("files", nargs="+", help="List of image files")
-    args = parser.parse_args()
+    parser = argparse.ArgumentParser(prog="img2term", description="view images in your terminal")
+    parser.add_argument("file", nargs="+", help="an image file, or a directory containing image files")
+    parser.add_argument("-n", "--nostatus", action="store_false", help="hide the status bar")
+    args = parser.parse_args()        
 
-    if len(args.files) == 0:
+    if len(args.file) == 0:
         print("img2term: No files specified")
         os._exit(1)
 
-    for file in args.files:
+    for file in args.file:
         file_name = str(file)
         load_images(file_name)
 
@@ -164,8 +177,10 @@ def main():
         print("img2term: No images to display")
         os._exit(1)
     
-    global resize_detected, needs_render, img_idx, scaled_images, rendered_images
+    global resize_detected, needs_render, show_status, img_idx, scaled_images, rendered_images
     signal.signal(signal.SIGWINCH, on_resize)
+
+    show_status = args.nostatus
 
     with term.fullscreen(), term.cbreak(), term.hidden_cursor():
         while True:
@@ -176,13 +191,19 @@ def main():
                     resize_detected = False
                 if img_idx not in scaled_images.keys():
                     scaled_images[img_idx] = resize_img(images[img_idx])
-                    mid_y = (term.height * 2 - scaled_images[img_idx].height) // 2
-                    if mid_y % 2 == 0:
+                    vy = term.height
+                    if show_status:
+                        vy -= 1
+                    mid_y = (vy * 2 - scaled_images[img_idx].height) // 2
+                    if mid_y % 2 != 0:
                         rendered_images[img_idx] = render_img_offset(scaled_images[img_idx])
                     else:
                         rendered_images[img_idx] = render_img(scaled_images[img_idx])
-                status = render_status(file_names[img_idx], scaled_images[img_idx], images[img_idx])
-                output = term.home + term.clear + rendered_images[img_idx] + status + term.clear_eos
+                if show_status:
+                    status = render_status(file_names[img_idx], scaled_images[img_idx], images[img_idx])
+                else:
+                    status = ""
+                output = term.home + term.clear + rendered_images[img_idx] + status
                 print(output, end="", flush=True)
                 needs_render = False
                     
@@ -197,6 +218,10 @@ def main():
                   and len(images) > 1 and img_idx > 0:
                 img_idx -= 1
                 needs_render = True
+            elif val.lower() == "s":
+                show_status = not show_status
+                needs_render = True
+                resize_detected = True
 
 
 if __name__ == "__main__":
